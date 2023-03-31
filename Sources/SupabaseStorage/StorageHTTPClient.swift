7af7edf1341301944d5db93bc3bd5e802d24a680
Ryan Burgoyne <ryan@skyglass.com>
2023-03-31 13:04:14 -0600

    Fix Linux support

--- Sources/SupabaseStorage/StorageHTTPClient.swift
+++ Sources/SupabaseStorage/StorageHTTPClient.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public protocol StorageHTTPClient {
   func fetch(_ request: URLRequest) async throws -> (Data, HTTPURLResponse)
